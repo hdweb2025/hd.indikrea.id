7ceb087e2a5731bfe4a5a8170426b26902602e0c
Husen Nh <hd.website19@gmail.com>
2026-03-09 21:57:23 +0700

    Update config.php

--- config.php
+++ config.php
@@ -6,4 +6,5 @@ $db   = "koperasi_mp";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) { die("Koneksi Gagal: " . mysqli_connect_error()); }
+mysqli_set_charset($conn, 'utf8mb4');
 ?>
