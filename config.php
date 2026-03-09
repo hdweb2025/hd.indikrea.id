0606306a798840dad39fecad778c40c6d3c20a08
Husen Nh <hd.website19@gmail.com>
2026-03-09 21:51:28 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "koperasi_mp";
+$pass = "CpAz:x!2";
+$db   = "koperasi_mp";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) { die("Koneksi Gagal: " . mysqli_connect_error()); }
+?>
