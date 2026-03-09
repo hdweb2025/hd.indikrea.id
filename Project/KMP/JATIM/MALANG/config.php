8edb4abac712388e10750c2db11ef0465e244683
Husen Nh <hd.website19@gmail.com>
2026-03-09 22:21:31 +0700

    Update config.php

--- Project/KMP/JATIM/MALANG/config.php
+++ Project/KMP/JATIM/MALANG/config.php
@@ -4,7 +4,11 @@ $user = "koperasi_mp";
 $pass = "CpAz:x!2";
 $db   = "koperasi_mp";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
-if (!$conn) { die("Koneksi Gagal: " . mysqli_connect_error()); }
-mysqli_set_charset($conn, 'utf8mb4');
+$conn = @mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    $db_error = "DB connection failed";
+    error_log("DB connection failed: " . mysqli_connect_error());
+} else {
+    mysqli_set_charset($conn, 'utf8mb4');
+}
 ?>
